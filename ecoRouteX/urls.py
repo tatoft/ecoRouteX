e35ea8389d4eb0599f25b05c0ebc2407bd886e94
Tato <italoluna23@gmail.com>
2024-11-11 16:24:34 -0500

    add home page

--- ecoRouteX/urls.py
+++ ecoRouteX/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("authentication.urls")),
+    path("",include("home.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
